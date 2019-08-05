1cf544daf28cc0a495720591f9a92bc56963450d
egilhuber <egilhuber@gmail.com>
2019-08-05 15:32:23 -0500

    added ability to delete a hero from the table

--- Controllers/SuperheroController.cs
+++ Controllers/SuperheroController.cs
@@ -24,9 +24,9 @@ namespace Superheroes.Controllers
         }
 
         // GET: Superhero/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(Superhero superhero)
         {
-            return View();
+            return View(superhero);
         }
 
         // GET: Superhero/Create
@@ -57,7 +57,17 @@ namespace Superheroes.Controllers
         {
             try
             {
-                return View();
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+
+                }
+                Superhero superhero = db.Heroes.Find(id);
+                if (superhero == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(superhero);
             }
             catch
             {
@@ -68,24 +78,36 @@ namespace Superheroes.Controllers
 
         // POST: Superhero/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, Superhero superhero)
+        public ActionResult Edit(int? id, Superhero superhero)
         {
-            try
+            if (ModelState.IsValid)
             {
-                // TODO: Add update logic here
-
+                db.Entry(superhero).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            catch
-            {
-                return View();
-            }
+            return View(superhero);
+            //try
+            //{
+            //    // TODO: Add update logic here
+            //    var hero =
+            //        (from h in db.Heroes
+            //         where h.Id == superhero.Id
+            //         select h).FirstOrDefault();
+            //    hero.HeroName = ""; //needs to read input from html ?no?
+            //    db.SaveChanges();
+            //    return RedirectToAction("Index");
+            //}
+            //catch
+            //{
+            //    return View();
+            //}
         }
 
         // GET: Superhero/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult Delete(Superhero superhero)
         {
-            return View();
+            return View(superhero);
         }
 
         // POST: Superhero/Delete/5
@@ -95,6 +117,12 @@ namespace Superheroes.Controllers
             try
             {
                 // TODO: Add delete logic here
+                Superhero superhero1 =
+                    (from h in db.Heroes
+                     where h.Id == id
+                     select h).First();
+                db.Heroes.Remove(superhero1);
+                db.SaveChanges();
 
                 return RedirectToAction("Index");
             }
